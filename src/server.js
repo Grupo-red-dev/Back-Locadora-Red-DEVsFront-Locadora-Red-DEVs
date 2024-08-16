02dbbbeb184ea90389334c870170f2142c8c9c89
PSjoon <pedrosantos.joon@gmail.com>
2024-08-16 19:09:07 -0300

    feat: Começado a integração ao front end
    Instalar dotenv
    Iniciado testes
    
    Co-authored-by: 0o0Musiquinha <0o0Musiquinha@users.noreply.github.com>
    Co-authored-by: Gabriel Viana Santos <Viana-Gabriel@users.noreply.github.com>

--- src/server.js
+++ src/server.js
@@ -1,7 +1,22 @@
-import app from './app.js';
+import app from "./app.js"
+import dotenv from "dotenv"
 
-const port = process.env.PORT || 3000;
+dotenv.config()
+const port = process.env.PORT || 3001
 
 app.listen(port, (res) => {
-    console.log(`Servidor rodando na porta: http://localhost:${port}`)
+  console.log(`Servidor rodando na perta: http://localhost:${port}`)
 })
+
+// async function loginUser(data: any) {
+//   try {
+//     const response = await api.post("/logar", {
+//       email: data.email,
+//       password: data.password,
+//     })
+
+//     routes.push("/")
+//   } catch (error) {
+//     window.location.search = "error=userNotExisted"
+//   }
+// }
